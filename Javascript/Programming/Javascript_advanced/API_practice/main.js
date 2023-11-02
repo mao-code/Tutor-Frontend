2ee88fc33453c8204248084c5ea809f0b8487de7
MaoXun <a22908352@gmail.com>
2023-11-02 21:14:04 +0800

    upload API practice

---
+++ Javascript/Programming/Javascript_advanced/API_practice/main.js
@@ -0,0 +1,64 @@
+
+
+//window.addEventListener('load', ()=>{})
+$(document).ready(()=>{
+    //document.getElementById("first-btn");
+    var firstBtn = $("#first-btn").get(0); // $() -> jquery function, $().get(index) -> DOM element
+    $("#first-btn").on('click', ()=>console.log("clicked!"))
+
+    console.log(firstBtn);
+});
+
+function jqueryAjax()
+{
+    // AJAX
+    // call API -> Web API -> 一串網址去跟伺服器拿資料
+    // HTTP Request
+    
+    // HTTP Method
+    // GET, POST, PUT,      PATCH,      DELETE
+    // 查詢, 新增, 更新（全）, 更新（部分）, 刪除
+    
+    // JSON -> Javascript object -> 資料格式 -> 網路傳輸資料的一種資料格式
+    // example API: https://www.7timer.info/bin/astro.php?lon=113.2&lat=23.1&ac=0&unit=metric&output=json&tzshift=0
+    $.ajax({
+        url: "https://api.agify.io/?name=bella",
+        type: "GET",
+        success: function(res){
+            console.log(res);
+        }
+    });    
+}
+
+function fetchAPI()
+{
+    fetch("https://api.agify.io/?name=bella")
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(err);
+    });
+
+    // fetch(apiUrl, {
+    //     method: 'POST',  // or 'PUT', 'DELETE', etc.
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       // Other headers if needed
+    //     },
+    //     body: JSON.stringify({
+    //       key1: 'value1',
+    //       key2: 'value2',
+    //       // ... other data ...
+    //     })
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     console.log(data);
+    //   })
+    //   .catch(error => {
+    //     console.log('Error:', error);
+    // });
+}
+
--- Javascript/Programming/Javascript_basic/Dies/main.js
+++ Javascript/Programming/Javascript_basic/Dies/main.js
@@ -1,6 +1,7 @@
 var $buuton;
 var $tbody;
 var frequency = [0, 0, 0, 0, 0, 0];
+var sumFreq = 0;
 
 window.addEventListener('load', ()=>{
     $buuton = document.getElementById("roll");
@@ -25,11 +26,24 @@ function setImagesAndCalculateFrequency()
 
         //2. calculate frequency
         // randomNum = 1 -> frequency[0]++
-        frequency[randomNum-1]++;
+        frequency[randomNum-1]++; // O(n)
+        // O(1) -> Hashmap
+        /*
+            {
+                1: 0,
+                2: 0,
+                3: 0,
+                4: 0,
+                5: 0,
+                6: 0
+            }
+        */
 
         let fileName = `./pic/dice/die${randomNum}.png`;
         die.setAttribute('src', fileName); //設定屬性(attribute), setAttribute(name, value);
     }
+
+    sumFreq += 12; // O(1)
 }
 
 function setTable()
@@ -41,7 +55,14 @@ function setTable()
         tdFreq.innerText = frequency[i];
 
         // percentage
-        
+        // frequency[i] / sum(frequency)
+        // let sumFreq = frequency.reduce((accumulator, currValue) => {
+        //     return accumulator + currValue;
+        // }, 0); // O(n)
+
+        let percentage = frequency[i] /  sumFreq;      
+        let tdPercent = tr_i.children[2];
+        tdPercent.innerText = `${(percentage*100).toFixed(2)}%`;
     }
 }
 
